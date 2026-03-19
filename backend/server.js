6a2d06f1c499ad04a96e0bd8498ae227a44993b7
kcisner <kevingael.cisneros047@gmail.com>
2026-03-19 15:55:13 -0600

    Frontend completed with React and Chakra UI

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
 import productRoutes from './routes/product.route.js';
+import { setServers } from "node:dns/promises";
 
+setServers(["1.1.1.1", "8.8.8.8"]);
 dotenv.config();
 
 const app = express();
@@ -13,7 +15,7 @@ app.use(express.json()); // Allows to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
     console.log("Server started at http://localhost:" + PORT);
 });
